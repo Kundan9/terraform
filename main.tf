3e06032acbec1e98a3cfa941bcb4d44ac254da6e
Kundan9 <kundan579p@hotmail.com>
2021-09-11 11:08:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 ## ResourceGroup
+
+#Azure Provider
+provider "azurerm" {
+  version = "=2.28.0"
+  features {}
+}
+
+
+
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.region_name
